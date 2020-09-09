52ac251fa30072f06d77916b5c8e20217c947ada
dindesrikanth <srikanthpatel132@gmail.com>
2020-09-09 16:51:31 +0530

    use serilization for better parsing

--- app/src/main/java/com/example/questionpaper/Response/mytests/UpcomingTestsResponse.java
+++ app/src/main/java/com/example/questionpaper/Response/mytests/UpcomingTestsResponse.java
@@ -1,4 +1,28 @@
 package com.example.questionpaper.Response.mytests;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class UpcomingTestsResponse {
+    @SerializedName("status")
+    @Expose
+    private String status;
+    @SerializedName("message_type")
+    @Expose
+    private String messageType;
+    @SerializedName("message_to")
+    @Expose
+    private String messageTo;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getMessageType() {
+        return messageType;
+    }
+
+    public String getMessageTo() {
+        return messageTo;
+    }
 }
