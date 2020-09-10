07c39a4e7ba2c896e0ecf973e863b722cbeb1ea8
kunigirirahul@gmail..com <patil@1234#>
2020-09-10 22:17:35 +0530

    details Analyis

---
+++ app/src/main/java/com/example/questionpaper/Screens/mytest/DetailsAnalysisFragment.java
@@ -0,0 +1,23 @@
+package com.example.questionpaper.Screens.mytest;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.questionpaper.R;
+
+
+public class DetailsAnalysisFragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view  = inflater.inflate(R.layout.fragment_details_analysis, container, false);
+        return view;
+    }
+}
