76885cead7023a78c334291b30e62e9bc05435cc
dindesrikanth <srikanthpatel132@gmail.com>
2020-09-22 21:00:45 +0530

    Added menu logic

--- app/src/main/java/com/example/questionpaper/Activity/ContainerActivity.java
+++ app/src/main/java/com/example/questionpaper/Activity/ContainerActivity.java
@@ -1,24 +1,43 @@
 package com.example.questionpaper.Activity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.questionpaper.R;
 import com.example.questionpaper.Screens.mytest.MyTestsLandingFragment;
+import com.example.questionpaper.Screens.mytest.UpComingTestFragment;
+import com.google.android.material.navigation.NavigationView;
 
 public class ContainerActivity extends AppCompatActivity {
     FragmentManager fManager;
     FragmentTransaction tr;
+
+    // Navigation drawer
+    private DrawerLayout mDrawer;
+    private Toolbar toolbar;
+    private NavigationView nvDrawer;
+    // Make sure to be using androidx.appcompat.app.ActionBarDrawerToggle version.
+    private ActionBarDrawerToggle drawerToggle;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.container_activity);
+        // Hide ActionBar
+        setNavigationDrawerLayout();
+
         fManager= getSupportFragmentManager();
         displayFragment(0);
     }
@@ -48,6 +67,82 @@ public class ContainerActivity extends AppCompatActivity {
     public void profileIconClick(View v){
         Toast.makeText(getApplicationContext(),"Icon clicked...",Toast.LENGTH_LONG).show();
     }
+    private void setNavigationDrawerLayout() {
+        // Set a Toolbar to replace the ActionBar.
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // This will display an Up icon (<-), we will replace it with hamburger later
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+       /* if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }*/
+        // Find our drawer view
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        // Find our drawer view
+        nvDrawer = (NavigationView) findViewById(R.id.nvView);
+        // Setup drawer view
+        setupDrawerContent(nvDrawer);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // The action bar home/up action should open or close the drawer.
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawer.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+    }
+
+    public void selectDrawerItem(MenuItem menuItem) {
+        // Create a new fragment and specify the fragment to show based on nav item clicked
+        Fragment fragment = null;
+        Class fragmentClass = null;
+        switch(menuItem.getItemId()) {
+            case R.id.nav_first_fragment:
+                //Toast.makeText(getApplicationContext(),"first clicked...",Toast.LENGTH_SHORT).show();
+                fragmentClass = UpComingTestFragment.class;
+                break;
+            /*case R.id.nav_second_fragment:
+                //fragmentClass = SecondFragment.class;
+                break;
+            case R.id.nav_third_fragment:
+                //fragmentClass = ThirdFragment.class;
+                break;*/
+            default:
+                fragmentClass = UpComingTestFragment.class;
+        }
+
+        if(fragmentClass != null){
+            try {
+                fragment = (Fragment) fragmentClass.newInstance();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            // Insert the fragment by replacing any existing fragment
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.containerLayout, fragment).commit();
+        }
+        // Highlight the selected item has been done by NavigationView
+        menuItem.setChecked(true);
+        // Set action bar title
+        setTitle(menuItem.getTitle());
+        // Close the navigation drawer
+        mDrawer.closeDrawers();
+    }
+
 
 
 }
